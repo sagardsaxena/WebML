4306c6b779a90ec35ed9b288846c29c190ffed23
Sagar Saxena <ssaxena1@umd.edu>
2021-11-13 02:21:01 -0500

    Load pre-trained model weights

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 var reader = new FileReader();
+var model = null;
+tf.loadLayersModel('model/model.json').then(e => model=e);
+
 reader.onload = function (e) {
+	// Update the image on the browser
     $('#image').attr('src', e.target.result);
+
+    // Load the image as a tensorflow array
     var img = tf.browser.fromPixels($('#image')[0]);
 }
 
