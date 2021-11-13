5984501bca16b945d0405a6294351878a044ccc6
Sagar Saxena <ssaxena1@umd.edu>
2021-11-13 02:30:58 -0500

    preprocess image

--- script.js
+++ script.js
@@ -1,13 +1,25 @@
 var reader = new FileReader();
 var model = null;
+var img = null;
+var scale = tf.scalar(1/255);
+// Load model
 tf.loadLayersModel('model/model.json').then(e => model=e);
 
+// Process new image
 reader.onload = function (e) {
 	// Update the image on the browser
     $('#image').attr('src', e.target.result);
 
     // Load the image as a tensorflow array
-    var img = tf.browser.fromPixels($('#image')[0]);
+    img = tf.browser.fromPixels($('#image')[0]);
+
+    // Resize image to 64 x 64
+    img = tf.image.resizeBilinear(img, [64, 64]);
+
+    // Scale between 0 and 1
+    img = img.mul(scale)
+
+    console.log(img)
 }
 
 function onImageUpload(){
